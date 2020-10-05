d381002926f06093f4202bca5e5cefae9e04ac60
maqsood <maqsoodhamd37@gmail.com>
2020-10-05 12:39:45 +0530

    [Maqsood].add: created the UserRegistration and UserRegistrationTest classes

--- src/main/java/com/org/day6UserRegistrationProblem/UserRegistration.java
+++ src/main/java/com/org/day6UserRegistrationProblem/UserRegistration.java
@@ -4,10 +4,10 @@ package com.org.day6UserRegistrationProblem;
  * Hello world!
  *
  */
-public class App 
+public class UserRegistration 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to the User Registration Program." );
     }
 }
