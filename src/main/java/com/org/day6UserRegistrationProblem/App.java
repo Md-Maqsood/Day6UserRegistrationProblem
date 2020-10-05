e9058dc29437b229c6bb5e63684382bb9c458f9e
maqsood <maqsoodhamd37@gmail.com>
2020-10-05 12:21:02 +0530

    [Maqsood].add: created the .gitignore file

---
+++ src/main/java/com/org/day6UserRegistrationProblem/App.java
@@ -0,0 +1,13 @@
+package com.org.day6UserRegistrationProblem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
